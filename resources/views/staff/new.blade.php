0c2d10af30a1a993333b2065fa5cdd69cd96e500
okayan28 <xxxx.xoxox@gmail.com>
2021-05-23 16:07:11 +0900

    スタッフを追加する画面を作ってる途中

--- resources/views/staff/new.blade.php
+++ resources/views/staff/new.blade.php
@@ -1,6 +1,11 @@
 <x-layout>
     {{-- TODO: がんばってフォームを書く --}}
-    <form>
-        <input type="text">
+    <form action="">
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                スタッフ名を追加して下さい。
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none foucus:shadow-outline" id="name" type="text" placeholder="スタッフ名">
+        </div>
     </form>
 </x-layout>
