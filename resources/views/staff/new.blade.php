b6c26ba777b0db4a3aed02bc4bf7ead5754887f2
okayan28 <xxxx.xoxox@gmail.com>
2021-05-27 22:04:49 +0900

    エラーメッセージが出るようになった

--- resources/views/staff/new.blade.php
+++ resources/views/staff/new.blade.php
@@ -2,32 +2,32 @@
     <h1 class="text-3xl my-8">スタッフ追加</h1>
     <form method="POST" action="/staffs/new/confirm">
         @csrf
-        @if($errors->any())
-        <div>
-            <ul>
-                @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
                 スタッフ名を入力して下さい。
             </label>
             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none foucus:shadow-outline" name="name" id="name" type="text" placeholder="スタッフ名">
+            @if ($errors->has('name'))
+                <div class="text-red-700 text-xs ml-2">{{$errors->first('name')}}</div>
+            @endif
         </div>
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                 パスワードを入力して下さい。
             </label>
             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none foucus:shadow-outline" name="password" id="password" type="password" placeholder="パスワード">
+            @if ($errors->has('password'))
+                <div class="text-red-700 text-xs ml-2">{{$errors->first('password')}}</div>
+            @endif
         </div>
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="password2">
                 パスワードをもう一度入力して下さい。
             </label>
             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none foucus:shadow-outline" name="password2" id="password2" type="password" placeholder="パスワード（確認）">
+            @if ($errors->has('password2'))
+            <div class="text-red-700 text-xs ml-2">{{$errors->first('password2')}}</div>
+            @endif
         </div>
         <div class="mb-4">
             <a href="#" class="inline-flex items-center bg-gray-400 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
