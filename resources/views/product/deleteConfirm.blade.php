a6b38db20fc966b7614792e27c52739625134c5d
okayan28 <xxxx.xoxox@gmail.com>
2021-08-19 08:06:57 -0500

    削除確認画面で画像が表示できた。

--- resources/views/product/deleteConfirm.blade.php
+++ resources/views/product/deleteConfirm.blade.php
@@ -13,6 +13,14 @@
             <p>&yen;{{$product['price']}}</p>
         </div>
         <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2">画像</label>
+            @if ($product['gazou_path'])
+            <img class="max-w-md" src="{{asset($product['gazou_path'])}}">
+            @else
+            <img class="max-w-md" src="{{asset($product->get_gazou_path())}}">
+            @endif
+        </div>
+        <div class="mb-4">
             <a href="/products/{{$product['id']}}" class="inline-flex items-center bg-gray-400 hover:bg-gray-700 text-white hover:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                 戻る
             </a>
